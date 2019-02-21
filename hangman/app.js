c1914eca60364a99a98b38711d72f443a56779f5
simon-jonsson <simon@melonia.se>
2019-02-21 18:41:22 +0100

    adding play game method

--- hangman/app.js
+++ hangman/app.js
@@ -2,6 +2,8 @@
  * Starting point of the app
  * Handles a game of Hangman
  * Author Simon Jonsson <sjovl08>
+ *
+ * Initiate game with command 'npm start'
  */
 
 const Hangman = require('./src/js/Hangman.js')
--- hangman/src/js/Hangman.js
+++ hangman/src/js/Hangman.js
@@ -10,6 +10,7 @@ const readline = require('readline').createInterface({
 class Hangman {
   constructor () {
     this.letters = []
+    this.currentLetter = ''
     this.randomWord = randomWord
     this.currentWord = {
       word: '',
@@ -17,12 +18,17 @@ class Hangman {
     }
     this.readline = readline
     this.player1 = {
-      name: ''
+      name: 'Player 1',
+      guesses: 10
     }
     this.player2 = {
-      name: '',
+      name: 'Player 2',
+      guesses: 10,
       multiplayer: false
     }
+    this.currentPlayer = this.player1
+    this.progress = []
+    this.numberOfGuesses = 0
   }
   /**
    * Initiate the game by calling for setOption method
@@ -34,6 +40,7 @@ class Hangman {
    * Set options for game
    */
   setOption () {
+    this.currentPlayer = this.player1
     console.log('1. Set nickname')
     console.log('2. Change to two players')
     console.log('3. Start Game')
@@ -44,11 +51,16 @@ class Hangman {
       } else if (choice === '2') {
         this.twoPlayers()
       } else if (choice === '3') {
-        this.setWord()
+        this.startGame()
       } else if (choice === '4') {
         this.quitGame()
       } else {
-        console.log('Wrong input')
+        console.log('\n')
+
+        console.log('WRONG INPUT! CHOOSE A NUMBER BETWEEN 1-4')
+        console.log('\n')
+
+        this.setOption()
       }
     })
   }
@@ -58,8 +70,8 @@ class Hangman {
   setWord () {
     let underScore = '_'
     this.currentWord.word = Array.from(this.randomWord())
-    console.log(this.currentWord.word)
-    console.log(underScore.repeat(this.currentWord.word.length))
+    this.progress = Array.from(underScore.repeat(this.currentWord.word.length))
+    console.log(this.progress.join(' '))
     this.guess()
   }
   /**
@@ -79,25 +91,61 @@ class Hangman {
    * Method for starting the game
    */
   startGame () {
+    this.currentWord.solved = false
+    this.progress = []
+    this.letters = []
+    this.numberOfGuesses = 0
+    console.log('\n')
+
+    console.log('Welcome to the game of Hangman! Start by guessing a letter')
     this.setWord()
   }
   /**
    * Method for handling guesses
    */
   guess () {
-    console.log(this.letters)
+    let index = 0
+
     readline.question('Guess a letter: ', (letter) => {
       this.letters.push(letter)
       this.currentWord.word.forEach(symbol => {
         if (symbol === letter) {
-          console.log('match!')
+          this.progress[index] = letter
         }
+        index++
       })
-      this.checkSolved()
+
+      console.log(this.progress.join(' '))
+      console.log(`Letters used: ${this.letters}`)
+      this.checkSolved(letter)
     })
   }
-  checkSolved () {
+  checkSolved (letter) {
+    let strProgress = this.progress.join(' ')
+    let strWord = this.currentWord.word.join(' ')
+    if (!strWord.includes(letter)) {
+      this.wrongGuess()
+    }
+    if (strProgress === strWord) {
+      this.currentWord.solved = true
+      if (this.player2.multiplayer === true) {
+        console.log('\n')
+        console.log(`${this.player1.name} finishied with ${this.numberOfGuesses}`)
+        console.log('\n')
+        console.log(`${this.player2.name}'s turn!`)
+        this.currentPlayer = this.player2
+        this.startGame()
+      }
+      readline.question('You Won! Play again? (y/n): ', (answer) => {
+        if (answer === 'y') {
+          this.startGame()
+        } else {
+          this.setOption()
+        }
+      })
+    }
     if (this.currentWord.solved === false) {
+      // this.currentPlayer.guesses--
       this.guess()
     }
   }
@@ -119,6 +167,20 @@ class Hangman {
       this.setOption()
     })
   }
+  wrongGuess () {
+    this.numberOfGuesses++
+    console.log(`Strike ${this.numberOfGuesses}/10`)
+    if (this.numberOfGuesses === 10) {
+      readline.question('Bad luck! You got hanged! Play again? (y/n): ', (answer) => {
+        if (answer === 'y') {
+          this.startGame()
+        } else {
+          this.setOption()
+        }
+      })
+    }
+  }
 }
 
+// Exports
 module.exports = Hangman
